78772870550a1022ceb1e5f87ae79927f1588b87
autumnpeterson24 <petersonautumn24@gmail.com>
2025-02-08 12:46:35 -0700

    data-output + error handling with invalid tickers

--- peterson_stock.py
+++ peterson_stock.py
@@ -3,31 +3,36 @@ peterson_stock: A program that uses the requests library to retrieve the min, ma
 given ticker over the past 5 years and output it as a json file.
 
 By: Autumn Peterson
-Date: 4 Feb. 2025
+Date: 8 Feb. 2025
 
 * Disclaimer: *
 I used chatgpt.com to help me with the headers when it came to requesting from the url. I had trouble with getting it to work and the headers helped
 me actually be able to retrieve the data. I also used it to help me with appending data to a json file. I originally had my code just
 overwrite the json everytime instead of append when there were multiple. Chatgpt.com helped me to append each stock instead of overwrite the file each time
-when writing and only having the latest stock.
+when writing and only having the latest stock. I also used GeeksForGeeks.com to help me out with sys.arv() to take commands from the terminal
 
 """
 
-import requests
-from datetime import date
-import json
-import math
+import requests # do get requests from web
+from datetime import date # to get dates from last five years
+import json # formatting the data to return
+import sys # for argv[] and taking commands directly from terminal
 
 def extract_price_lst(raw_data_dict: dict)->list[float]:
     """ Takes in the full data received from the request and extracts the list of close prices that are converted to floats"""
 
     close_lst = []
 
+    if raw_data_dict.get('data') == None:
+        print("Error: Ticker symbol does not exist.")
+        return [0,0,0,0] # return an all 0 list if the ticker symbol is not recognized
+
     for data_rows in raw_data_dict['data']['tradesTable']['rows']:
         close_lst.append(data_rows['close'])
 
     close_lst = [float(p.replace("$", "")) for p in close_lst] # removing the '$' from the close prices and converting to floats for operations to be performed later
     return close_lst
+
 
 def download_data(ticker: str) -> dict:
     """ Requests data from https://api.nasdaq.com based on the given ticker and outputs a dictionary of the min, max, average, median of the 
@@ -53,36 +58,52 @@ def download_data(ticker: str) -> dict:
     print(f"Requesting: {base_url + path}") # check to make sure url is correct
     try:
         response = requests.get(base_url + path, headers=headers, timeout=10) #timeout means if it takes more than 10 sec to cut it off
-        print(f"Response received! Status code: {response.status_code}") # check if respose is properly received
+        print(f"Response received! Status code: {response.status_code}") # check if respose is properly received 
+        
+
         if "application/json" in response.headers.get("Content-Type", ""): # retrieve the json data we want to scrape data from
             data_dict = dict(response.json())
 
             close_prices = extract_price_lst(data_dict) # converts to a price list that is floats so numerical operations can be performed
 
-            
-            min_close = min(close_prices) # get the min close price
-            max_close = max(close_prices) # get the max close price
-            avg_close = sum(close_prices) / len(close_prices) # get the avg close price
+            if(close_prices != [0,0,0,0]):
 
-            sorted_close = sorted(close_prices)
+                min_close = min(close_prices) # get the min close price
+                max_close = max(close_prices) # get the max close price
+                avg_close = sum(close_prices) / len(close_prices) # get the avg close price
 
-            med_index = len(close_prices) // 2 # integer division so no indexing with a float
+                sorted_close = sorted(close_prices)
 
-            if((len(close_prices) / 2) % 2 !=0): # if it's odd there is a clean middle element to take
-                median = sorted_close[med_index] # if there is a clean middle we can directly take the median
+                med_index = len(close_prices) // 2
 
-            else:
-                median = (sorted_close[med_index - 1] + sorted_close[med_index]) / 2 # if there is no clean middle term, take the average of the middle 2
+                if((len(close_prices) / 2) % 2 != 0):
+                    median = sorted_close[med_index] # if there is a clean middle we can directly take the median
+
+                else:
+                    median = (sorted_close[med_index - 1] + sorted_close[med_index]) / 2 # if there is no clean middle term, take the average of the middle 2
                         
-            final_data = dict(ticker=ticker, min=min_close, max=max_close, average=avg_close, median=median) # the final data we want to save to the json
+                final_data = dict(ticker=ticker, min=min_close, max=max_close, average=avg_close, median=median) # the final data we want to save to the json
 
-            print(final_data)
-            #final_data = {}
 
+            else: # if there was an invalid ticker name then return an empty dict
+                final_data = {}
+
+
+            file_name = "stocks.json" # name of the output json file
+
+            try:
+                with open(file_name, "r") as json_file: # read existing data if the file exists so we can append
+                    existing_data = json.load(json_file)
+            except (FileNotFoundError): # add extra protection in case file is not found
+                existing_data = [] # existing data is empty if there is no previous json
+
+            existing_data.append(final_data) # appending the data
+
+            with open(file_name, "w") as json_file: # write to file
+                json.dump(existing_data, json_file, indent=3) # takes the dict data and dumps it into a json
 
             return final_data
- 
-        
+
 
     except requests.exceptions.Timeout: # if the request takes too long it times out and returns none
         print("Request timed out!")
@@ -95,9 +116,12 @@ def download_data(ticker: str) -> dict:
 
 # TESTING ================================================================================================
 
+if len(sys.argv) < 2: # checks to make sure an argument is passed
+    print("Invalid: Please put in at least one ticker name.")
+    sys.exit(1)
 
-ticker_lst = ["msft", "aapl", "tsla", "amzn"]
-# msft aapl tsla amzn
+
+ticker_lst = sys.argv[1:] # takes argument straight from terminal
 
 for ticker in ticker_lst:
     download_data(ticker)
